ba4788ac2bd8cbc6457ecd39a727c07ddd984728
Alessander <alessander.logistica@gmail.com>
2019-12-27 22:58:53 -0300

    initial commit

---
+++ src/test/java/br/com/alessanderleite/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
