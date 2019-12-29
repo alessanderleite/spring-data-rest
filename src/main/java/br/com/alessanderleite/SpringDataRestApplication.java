d94ebd161eb2388160eb01837a180b40c3b828c8
Alessander <alessander.logistica@gmail.com>
2019-12-28 22:46:41 -0300

    call CommandLineRunner

--- src/main/java/br/com/alessanderleite/SpringDataRestApplication.java
+++ src/main/java/br/com/alessanderleite/SpringDataRestApplication.java
@@ -1,13 +1,29 @@
 package br.com.alessanderleite;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alessanderleite.entities.User;
+import br.com.alessanderleite.repositories.UserRepository;
 
 @SpringBootApplication
 public class SpringDataRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataRestApplication.class, args);
+	}
+	
+	CommandLineRunner initialize(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Robert", "Nataly", "Helen", "Mary").forEach(name -> {
+				User user = new User(name);
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
 	}
 
 }
