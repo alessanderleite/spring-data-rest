c30d5d4d422e7913b27257232cde9cc87c6ac820
Alessander <alessander.logistica@gmail.com>
2019-12-29 00:08:22 -0300

    add the @Bean annotation

--- src/main/java/br/com/alessanderleite/SpringDataRestApplication.java
+++ src/main/java/br/com/alessanderleite/SpringDataRestApplication.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import br.com.alessanderleite.entities.User;
 import br.com.alessanderleite.repositories.UserRepository;
@@ -16,9 +17,10 @@ public class SpringDataRestApplication {
 		SpringApplication.run(SpringDataRestApplication.class, args);
 	}
 	
+	@Bean
 	CommandLineRunner initialize(UserRepository userRepository) {
 		return args -> {
-			Stream.of("John", "Robert", "Nataly", "Helen", "Mary").forEach(name -> {
+			Stream.of("John", "Robert", "Nataly", "Helen", "Mary", "Alexandre", "Thom").forEach(name -> {
 				User user = new User(name);
 				userRepository.save(user);
 			});
